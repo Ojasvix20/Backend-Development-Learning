7258b17bd9e3ecd3372b4cdd3b9f02fa18f67592
Ojasvix20 <ojasvi.bakshi@gmail.com>
2026-07-10 16:27:48 +0530

    add

--- ToDo APP/index.js
+++ ToDo APP/index.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('App is running successfully');
-    
-})
+//load config from env
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+//middleware to parse json request body
+app.use(express.json());
+
+//import routes for TODO API
+
+const todoRoutes = require("./routes/todos");
