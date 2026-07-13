3b140cfe415552f18968fdd04c7a6a0494176538
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2026-07-13 23:04:13 +0530

    new

--- Notes App/src/app.js
+++ Notes App/src/app.js
@@ -11,12 +11,27 @@ note={
 const notes = [];
 app.use(express.json()); //middleware to make the body of request readable for express
 
+// POST /notes
 app.post("/notes", (req, res) => {
   notes.push(req.body);
 
   res.status(201).json({
-    message:"note created successfully"
+    message: "note created successfully",
   });
 });
+
+// GET /notes
+app.get("/notes", (req,res) => {
+  res.status(200).json({
+    message: "notes fetched successfully",
+    notes: notes,
+  });
+});
+
+//DELETE a note:
+
+app.delete("/notes/index",(req,res)=>{
+  
+})
 
 module.exports = app;
