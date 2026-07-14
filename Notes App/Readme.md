83d82a2949e47404afcf47bd862b04f228ef9f49
Ojasvix20 <ojasvi.bakshi@gmail.com>
2026-07-14 23:19:03 +0530

    add

---
+++ Notes App/Readme.md
@@ -0,0 +1,17 @@
+# HTTP Request Methods:
+
+### GET:
+
+Read/ fetch data from the server
+
+### POST:
+
+Send data to the server
+
+### DELETE:
+
+Delete data from the server
+
+### PATCH:
+
+Update data that exists on the server
--- Notes App/src/app.js
+++ Notes App/src/app.js
@@ -8,10 +8,13 @@ note={
     description:"this is my first note"
 }
 */
-const notes = [];
+const notes = []; //array of notes
+//  since theres no database rn, if server restarts, the data is gone. because the array exists on ram... and every restart means new ram assigned.
+
 app.use(express.json()); //middleware to make the body of request readable for express
 
 // POST /notes
+
 app.post("/notes", (req, res) => {
   notes.push(req.body);
 
@@ -21,17 +24,38 @@ app.post("/notes", (req, res) => {
 });
 
 // GET /notes
-app.get("/notes", (req,res) => {
+
+app.get("/notes", (req, res) => {
   res.status(200).json({
     message: "notes fetched successfully",
     notes: notes,
   });
 });
 
-//DELETE a note:
+//DELETE a note -> (delete /notes/:index)
 
-app.delete("/notes/index",(req,res)=>{
-  
-})
+app.delete("/notes/:index", (req, res) => {
+  //over here, full colon tells that the value is dynamic
+
+  const index = req.params.index;
+
+  delete notes[index];
+
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+
+//PATCH - update existing data on the given index
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const description = req.body.description;
+
+  notes[index].description = description;
+  res.status(200).json({
+    message: `note ${index} updated successfully`,
+  });
+});
 
 module.exports = app;
