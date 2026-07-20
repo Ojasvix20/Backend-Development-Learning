23b8265f512d9d845b30e10a5b61c21b1d4cc85e
Ojasvix20 <ojasvi.bakshi@gmail.com>
2026-07-21 03:37:27 +0530

    add

--- Project/src/db/db.js
+++ Project/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   await mongoose.connect(
-    "mongodb+srv://Ojasvi:YgxPe9KEJofSy7GM@cluster0.zdwd0lc.mongodb.net/Project1",
+    "mongodb+srv://Ojasvi:***@cluster0.zdwd0lc.mongodb.net/Project1",
   );
   console.log("Connected to DB");
 }
