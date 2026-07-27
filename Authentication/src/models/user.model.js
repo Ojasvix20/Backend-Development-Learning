85acc1790fca11b3a76a1c888e30fda8428333cb
Ojasvix20 <ojasvi.bakshi@gmail.com>
2026-07-28 01:20:47 +0530

    add Auth

---
+++ Authentication/src/models/user.model.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+const mognoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
